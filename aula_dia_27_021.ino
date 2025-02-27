6be51637913e513ec4b5c83eedadb7d69c018cf9
LUÍS MIGUEL PEREIRA LIMA <luiskasami@gmail.com>
2025-02-27 10:49:31 -0300

    atividade

---
+++ aula_dia_27_021.ino
@@ -0,0 +1,76 @@
+// C++ code
+//autor: Luis Miguel
+//DATA 27/02/2025
+
+int numeroX= -150;
+  
+  
+void setup()
+{
+Serial.begin(9600);
+  /*if(numeroX<= -1){
+  Serial.println("numero negativo");
+  }  
+  else if(numeroX>=1){
+  Serial.println("numero positivo");
+  }
+  */
+  
+   
+  
+  /*String nome = "Arlindo Cruz" ;
+    String cargo = "Empilhador";
+    float salario = 480;
+   
+  
+  
+
+ if (salario<1000){
+   salario = salario *1.1;
+ }
+  Serial.println(nome);
+  Serial.println(cargo);
+  Serial.println(salario);*/
+  
+  /*int salario =600 ;
+    
+    if (salario<500){
+  salario = salario *1.3;
+      Serial.println("funcionario tem direito ao reajuste");
+   }
+  else{
+  Serial.println("Nao tem direito ao reajsute");
+  }*/
+  
+  
+  /*int numeroA= 950;
+    int numeroB=28845;
+  if (numeroA>= numeroB){
+  Serial.print(numeroA);
+    Serial.println(" , A eh maior");
+  }
+  else {
+    Serial.print(numeroB);
+    Serial.println(" , B eh maior");
+      }*/
+  /*float n1 = 10, n2 = 9, n3 = 10, n4 = 9, media;
+  media = (n1+n2+n3+n4) / 4.0;
+  if(media >= 7){
+  Serial.println("Aprovado");
+  }
+  else if (media< 7 && media > 5){
+    Serial.println("Recuperacao");
+    
+    
+  }
+  else {
+    Serial.println("Reprovado");
+    }*/
+    
+  
+}
+
+void loop()
+{
+  
+}
